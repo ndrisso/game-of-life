a997ff5a95f540774f5e5863b8cbca1572ee81ff
Nicolas Risso <nicolas.risso@restorando.com>
2016-11-24 22:55:19 -0300

    Update app.js to distribute

--- dist/app.js
+++ dist/app.js
@@ -46,22 +46,106 @@
 
 	"use strict";
 
-	var _map = __webpack_require__(1);
+	var _engine = __webpack_require__(1);
 
-	var _map2 = _interopRequireDefault(_map);
+	var _engine2 = _interopRequireDefault(_engine);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var elem = document.getElementById("map");
 	var ctx = elem.getContext("2d");
+	var rows = 50;
+	var cols = 50;
 
-	var map = new _map2.default({ width: 500, height: 500, ctx: ctx });
-	map.initMap();
-
-	console.log("hi!");
+	(0, _engine2.default)({ rows: rows, cols: cols, ctx: ctx }).start();
+	console.log("started!");
 
 /***/ },
 /* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _map2 = __webpack_require__(2);
+
+	var _map3 = _interopRequireDefault(_map2);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var engine = function engine(mapProps) {
+
+	  var matrix = [];
+	  var _map = (0, _map3.default)(mapProps);
+	  var rows = mapProps.rows,
+	      cols = mapProps.cols;
+
+
+	  var randomizeMatrix = function randomizeMatrix() {
+	    for (var i = 0; i < rows; i++) {
+	      matrix[i] = [];
+	      for (var j = 0; j < cols; j++) {
+	        // Giving more weight to death cells
+	        var rand = Math.floor(Math.random() * 8);
+	        matrix[i][j] = rand === 0 ? 1 : 0;
+	      }
+	    }
+	  };
+
+	  var updateLife = function updateLife() {
+	    for (var i = 0; i < rows; i++) {
+	      for (var j = 0; j < cols; j++) {
+	        var alive = aliveNeightbors(i, j);
+	        var elem = matrix[i][j];
+
+	        if (alive === 3 && elem === 0) {
+	          matrix[i][j] = 1;
+	        } else if ((alive < 2 || alive > 3) && elem === 1) {
+	          matrix[i][j] = 0;
+	        }
+	      }
+	    }
+	  };
+
+	  var aliveNeightbors = function aliveNeightbors(row, col) {
+	    var fromRow = row - 1 < 0 ? row : row - 1;
+	    var fromCol = col - 1 < 0 ? col : col - 1;
+	    var toRow = row + 1 === rows ? row : row + 1;
+	    var toCol = col + 1 === cols ? col : col + 1;
+	    var alives = 0;
+
+	    for (var i = fromRow; i <= toRow; i++) {
+	      for (var j = fromCol; j <= toCol; j++) {
+	        if (matrix[i][j] === 1) {
+	          alives++;
+	        }
+	      }
+	    }
+
+	    return alives - matrix[row][col];
+	  };
+
+	  var start = function start() {
+	    randomizeMatrix();
+
+	    setInterval(function () {
+	      updateLife();
+	      _map.drawMap(matrix);
+	    }, 200);
+	  };
+
+	  return {
+	    start: start
+	  };
+	};
+
+	exports.default = engine;
+
+/***/ },
+/* 2 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -69,60 +153,29 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
+	var map = function map(_ref) {
+	  var rows = _ref.rows,
+	      cols = _ref.cols,
+	      ctx = _ref.ctx;
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	var Map = function () {
-	  function Map(_ref) {
-	    var width = _ref.width,
-	        height = _ref.height,
-	        ctx = _ref.ctx;
-
-	    _classCallCheck(this, Map);
-
-	    this._width = width;
-	    this._height = height;
-	    this._ctx = ctx;
-	  }
-
-	  _createClass(Map, [{
-	    key: "emptyMap",
-	    value: function emptyMap() {
-	      this._ctx.fillStyle = "white";
-	      this._ctx.fillRect(0, 0, this._width, this._height);
-	    }
-	  }, {
-	    key: "drawGrid",
-	    value: function drawGrid() {
-	      this._ctx.fillStyle = "black";
-	      for (var _i = 0; _i < this._width; _i += 10) {
-	        this._ctx.beginPath();
-	        this._ctx.moveTo(_i, 0);
-	        this._ctx.lineTo(_i, this._height);
-	        this._ctx.stroke();
-	      }
-	      for (var j = 0; j < this._height; j += 10) {
-	        this._ctx.beginPath();
-	        this._ctx.moveTo(0, i);
-	        this._ctx.lineTo(this._width, i);
-	        this._ctx.stroke();
+	  var drawMap = function drawMap(matrix) {
+	    for (var j = 0; j < rows; j++) {
+	      for (var i = 0; i < cols; i++) {
+	        ctx.fillStyle = matrix[j][i] === 1 ? "gray" : "white";
+	        ctx.fillRect(j * 10, i * 10, 10, 10);
+	        ctx.strokeStyle = "black";
+	        ctx.strokeRect(j * 10, i * 10, 10, 10);
 	      }
 	    }
-	  }, {
-	    key: "initMap",
-	    value: function initMap() {
-	      debugger;
-	      this.emptyMap();
-	      this.drawGrid();
-	    }
-	  }]);
+	  };
 
-	  return Map;
-	}();
+	  return {
+	    drawMap: drawMap
+	  };
+	};
 
-	exports.default = Map;
+	exports.default = map;
 
 /***/ }
 /******/ ]);
